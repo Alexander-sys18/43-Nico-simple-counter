bd765fd6479c73538cc07f14b3e2c78c3e8e5d27
Alexander-sys18 <119635571+Alexander-sys18@users.noreply.github.com>
2023-06-24 00:49:01 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	const valorQueCambia = useState()
+	const [color, setColor] = useState("green")
+
+	console.log("Retorno de useState:", valorQueCambia)
+
+	function generateRandomColor(){
+		const colors= ["red", "white", "pink", "black"]
+		const randomNumber= Math.floor(Math.random() * colors.length)
+		return colors[randomNumber]
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="w-100 vh-100 justify-content-center align-items-center d-flex" style={{background: color}}>
+			<button className="btn btn-primary"
+				onClick={() =>{
+					const newColor = generateRandomColor()
+					setColor(newColor)
+				}}
+			>Cambiar color</button>
 		</div>
-	);
+	)
 };
 
 export default Home;
